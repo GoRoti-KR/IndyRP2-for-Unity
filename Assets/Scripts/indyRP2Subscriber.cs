376723fb83c076e0dae91da1b106d25d7b6d0ff0
aqoudayo <imaku0417@gmail.com>
2024-10-17 17:42:42 +0900

    [241017] Joint State 구독 확인 작업 추가
    [241017] Joint State 구독시 정렬 문제가 있어 name 기반 확인 작업 추가

--- Assets/Scripts/indyRP2Subscriber.cs
+++ Assets/Scripts/indyRP2Subscriber.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 using JointStateMsg = RosMessageTypes.Sensor.JointStateMsg;
-using System.Threading;
 
 public class indyRP2Subscriber : MonoBehaviour
 {
@@ -16,6 +15,7 @@ public class indyRP2Subscriber : MonoBehaviour
     public ArticulationBody joint_7;
 
     private ArticulationBody[] joint;
+    private Dictionary<string, int> jointNameToIndex;
     private ROSConnection ros;
 
     // Start is called before the first frame update
@@ -24,7 +24,7 @@ public class indyRP2Subscriber : MonoBehaviour
         ros = ROSConnection.GetOrCreateInstance();
         ros.Subscribe<JointStateMsg>("/joint_states", Callback);
 
-        joint = new ArticulationBody[6];
+        joint = new ArticulationBody[7];
         joint[0] = joint_1;
         joint[1] = joint_2;
         joint[2] = joint_3;
@@ -32,15 +32,31 @@ public class indyRP2Subscriber : MonoBehaviour
         joint[4] = joint_5;
         joint[5] = joint_6;
         joint[6] = joint_7;
+
+        // joint 이름을 인덱스와 매핑
+        jointNameToIndex = new Dictionary<string, int>
+        {
+            { "joint0", 0 },
+            { "joint1", 1 },
+            { "joint2", 2 },
+            { "joint3", 3 },
+            { "joint4", 4 },
+            { "joint5", 5 },
+            { "joint6", 6 }
+        };
     }
 
     void Callback(JointStateMsg msg)
     {
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < msg.name.Length; i++)
         {
-            ArticulationDrive aDrive = joint[i].xDrive;
-            aDrive.target = Mathf.Rad2Deg * (float)msg.position[i];
-            joint[i].xDrive = aDrive;
+            // 이름에 해당하는 인덱스를 찾아 position 값을 적용
+            if (jointNameToIndex.TryGetValue(msg.name[i], out int jointIndex))
+            {
+                ArticulationDrive aDrive = joint[jointIndex].xDrive;
+                aDrive.target = Mathf.Rad2Deg * (float)msg.position[i];
+                joint[jointIndex].xDrive = aDrive;
+            }
         }
     }
 }
